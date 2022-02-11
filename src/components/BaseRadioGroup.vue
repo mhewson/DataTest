ec5db239df4f030a130ec2ae539b2baef24eb320
mhewson <mhewson@rei.com>
2022-02-11 12:03:22 -0800

    form components

---
+++ src/components/BaseRadioGroup.vue
@@ -0,0 +1,55 @@
+<template>
+  <fieldset>
+    <legend> {{ legend }}</legend>
+    <component
+      v-for="option in options"
+      :key="option.value"
+      :is="displayBlock ? 'div' : 'span'"
+      :class="{
+        inline: !displayBlock
+      }
+      "
+    >
+      <BaseRadio
+        :label="option.label"
+        :value="option.value"
+        :name="name"
+        :modelValue="modelValue"
+        @update:modelValue="$emit('update:modelValue', $event)"
+      />
+    </component>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  props: {
+    legend: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Array,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    modelValue: {
+      type: [String, Number],
+      required: true
+    },
+    displayBlock: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.inline {
+  margin-right: 20px;
+}
+</style>
